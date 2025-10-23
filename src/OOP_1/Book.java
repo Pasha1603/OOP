308aac96ecc2331b1c387164ba0bc457251b0d16
pasha <pasha.lysanov.02@mail.ru>
2025-10-23 18:42:32 +0300

    1

---
+++ src/OOP_1/Book.java
@@ -0,0 +1,38 @@
+package OOP_1;
+
+import java.util.Objects;
+
+public class Book {
+    String author;
+    String title;
+    int year;
+    boolean isRead;
+    public Book(String bookAuthor, String bookTitle,int bookYear){
+        author = bookAuthor;
+        title = bookTitle;
+        year = bookYear;
+        isRead = false;
+    }
+    public void displayInfo(){
+        System.out.println("Name:"+ title);
+        System.out.println("Author:"+ author);
+        System.out.println("Written:"+ year);
+
+    }
+    public void markAsRead() {
+        if (!isRead){
+            isRead = true;
+            System.out.println("Read it");
+        }
+
+        }
+        public void isOldBook(){
+        if(year<2000){
+            System.out.println("Old");
+        }
+        else {
+            System.out.println("New");
+        }
+        }
+    }
+
---
+++ src/OOP_1/Main.java
@@ -0,0 +1,24 @@
+package OOP_1;
+
+public class Main {static void main(String[]args){
+    Book book1 = new Book("Pushkin","Evgeny Onegin",1831);
+    Book book2 = new Book("Chehov","Krujovnik", 1898);
+    Book book3 = new Book("Sorokin","Nasty",2001 );
+    System.out.println("Book:");
+    book1.displayInfo();
+    book1.markAsRead();
+    book1.isOldBook();
+    System.out.println();
+
+    System.out.println("Book:");
+    book2.displayInfo();
+    book2.markAsRead();
+    book2.isOldBook();
+    System.out.println();
+
+    System.out.println("Book:");
+    book3.displayInfo();
+    book3.markAsRead();
+    book3.isOldBook();
+}
+}
